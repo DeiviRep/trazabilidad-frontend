1c511f2b73289649943a15a3657159849dff1de1
deiviRep <patzivargasdavid@gmail.com>
2026-04-06 11:01:09 -0400

    feat: updte rules eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,8 @@ const eslintConfig = [
         "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-unused-vars": "warn",
         "react-hooks/exhaustive-deps": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
